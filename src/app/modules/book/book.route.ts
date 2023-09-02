a9d7b698d1c0b3c9b0dd47eb9c767a05fc2ad138
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2023-09-02 07:09:35 +0300

    [done] implemented authorization for book route

--- src/app/modules/book/book.route.ts
+++ src/app/modules/book/book.route.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { BookValidation } from './book.validation';
 import { BookController } from './book.controller';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enum/user';
 
 const router = express.Router();
 
 router.post(
   '/create-book',
+  auth(ENUM_USER_ROLE.ADMIN),
   validateRequest(BookValidation.createBookZodSchema),
   BookController.createBook
 );
@@ -18,9 +21,10 @@ router
   .route('/:id')
   .get(BookController.getSingleBook)
   .patch(
+    auth(ENUM_USER_ROLE.ADMIN),
     validateRequest(BookValidation.updateBookZodSchema),
     BookController.updateBook
   )
-  .delete(BookController.deleteBook);
+  .delete(auth(ENUM_USER_ROLE.ADMIN), BookController.deleteBook);
 
 export const BookRoutes = router;
